876318288d83986a537a215fde7af59c2c02297a
serviatorem <103734921+serviatorem@users.noreply.github.com>
2022-12-21 23:40:23 +0300

    version 2.0

--- js/main.js
+++ js/main.js
@@ -13,6 +13,14 @@ const swiper = new Swiper(".mySwiper", {
     },
 });
 const pagination = document.querySelectorAll('.swiper-pagination-bullet');
+pagination[0].classList.add('logo');
+pagination[0].innerHTML = `О компании`;
+pagination[1].innerHTML = `Производство`;
+pagination[2].innerHTML = `Продукция`;
+pagination[3].innerHTML = `Для дилеров`;
+pagination[4].innerHTML = `Документация`;
+pagination[5].innerHTML = `Статьи`;
+pagination[6].innerHTML = `Контакты`;
 window.addEventListener('resize',function (){
     const windowWidth = window.innerWidth;
     if (windowWidth>1091){
