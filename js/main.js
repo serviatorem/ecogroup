630142ed435ab9e5110264c14e24a5c9154d6f77
serviatorem <103734921+serviatorem@users.noreply.github.com>
2022-12-21 23:36:29 +0300

    first version

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    rewind:true,
+    lazy: true,
+    simulateTouch:false,
+    navigation:{
+      nextEl:'.swiper-button-next',
+      prevEl:'.swiper-button-prev'
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+const pagination = document.querySelectorAll('.swiper-pagination-bullet');
+window.addEventListener('resize',function (){
+    const windowWidth = window.innerWidth;
+    if (windowWidth>1091){
+        pagination[0].classList.add('logo');
+        pagination[0].innerHTML = `О компании`;
+        pagination[1].innerHTML = `Производство`;
+        pagination[2].innerHTML = `Продукция`;
+        pagination[3].innerHTML = `Для дилеров`;
+        pagination[4].innerHTML = `Документация`;
+        pagination[5].innerHTML = `Статьи`;
+        pagination[6].innerHTML = `Контакты`;
+    }
+    if (windowWidth<1091 && pagination.length>0){
+        pagination.forEach(item=>{
+            item.innerHTML = '';
+        })
+    }
+})
+
